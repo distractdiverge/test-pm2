0d8f349e0ccdfe6672d13e5ae2831c91dd29aeda
Alex Lapinski <contact@alexlapinski.name>
2017-05-18 21:28:20 -0400

    Added simple nodejs/express app

---
+++ app/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({message: 'Hello World!'});
+});
+
+app.get('/environment', (req, res) => {
+    res.json(process.env);
+});
+
+app.listen(PORT, function () {
+  console.log(`Example app listening on port ${PORT}!`);
+});
